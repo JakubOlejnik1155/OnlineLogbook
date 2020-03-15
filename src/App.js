273d939d8bc1419679fd2fb663616ceccc3ad124
Jakub Olejnik <jakub.k.olejnik@gmail.com>
2020-03-15 16:22:42 +0100

    add hover effects to buttons

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import {Switch,Route} from "react-router-dom";
 import BadPath from './Components/BadPath';
 import LoginSite from './Components/LoginSite';
